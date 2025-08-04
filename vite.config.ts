f51bed0400d7c8be7e19f19d3aaeb40722f075c2
begum02 <aysebegumkoyuncu51@gmail.com>
2025-08-04 18:29:59 +0200

    vite config dosyasında build server ve preview ayarları

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      //API isteklerini farklı servera yönlendirir
-      '/api':{
-
-      }
-    }
+  build:{
+    outDir:'dist'
+  },
+    server: {
+    port: 3000,
+    open: true
+  },
+  preview: {
+    port: 4173
   }
 })
